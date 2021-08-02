28392be486e941d143dc44cf9c8eceb3e617abaf
Julia <julka.jaremkiw@gmail.com>
2021-08-02 11:17:10 +0200

    Add routing to tthe project

--- donations/src/App.js
+++ donations/src/App.js
@@ -1,10 +1,29 @@
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+import {Home} from "./components/Home/Home";
+import {Login} from "./components/Login/Login";
+import {Logout} from "./components/Logout/Logout";
 import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+            <Route path="/login">
+                <Login />
+            </Route>
+            <Route path="/logout">
+                <Logout />
+            </Route>
+        </Switch>
+      </Router>
   );
 }
 
